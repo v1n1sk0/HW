41a62a339e7323c07118409609c15caec3bfc391
v1n1sk0 <maksimserbakov484@gmail.com>
2025-03-12 15:37:05 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,113 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 1: Целые числа от 1 до 10
+
+        // Задача 1: Посчитать сколько месяцев потребуется, чтобы накопить 2 459 000 рублей
         System.out.println("Задача 1:");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
+        int savingsGoal = 2459000;  // Цель накоплений
+        int monthlyDeposit = 15000;  // Ежемесячный вклад
+        int currentSavings = 0;  // Текущая сумма накоплений
+        int months = 0;  // Счётчик месяцев
 
-        // Задача 2: Целые числа от 10 до 1
-        System.out.println("\nЗадача 2:");
-        for (int i = 10; i >= 1; i--) {
-            System.out.println(i);
+        while (currentSavings < savingsGoal) {
+            currentSavings += monthlyDeposit;
+            months++;
+            System.out.println("Месяц " + months + ", сумма накоплений равна " + currentSavings + " рублей");
         }
+        System.out.println("Для накопления " + savingsGoal + " рублей потребуется " + months + " месяцев.");
+        System.out.println(); // Переход на новую строку
 
-        // Задача 3: Четные числа от 0 до 17
-        System.out.println("\nЗадача 3:");
-        for (int i = 0; i <= 17; i += 2) {
-            System.out.println(i);
-        }
-
-        // Задача 4: Числа от 10 до -10 от большего к меньшему
-        System.out.println("\nЗадача 4:");
-        for (int i = 10; i >= -10; i--) {
-            System.out.println(i);
-        }
-
-        // Задача 5: Високосные года с 1904 по 2096
-        System.out.println("\nЗадача 5:");
-        for (int year = 1904; year <= 2096; year += 4) {
-            System.out.println(year + " год является високосным");
-        }
-
-        // Задача 6: Последовательность чисел 7, 14, 21, ..., 98
-        System.out.println("\nЗадача 6:");
-        for (int i = 7; i <= 98; i += 7) {
+        // Задача 2: Вывести числа от 1 до 10 с помощью цикла while и от 10 до 1 с помощью цикла for
+        System.out.println("Задача 2:");
+        // Цикл while
+        int i = 1;
+        while (i <= 10) {
             System.out.print(i + " ");
+            i++;
         }
-        System.out.println(); // Для перевода строки после вывода
+        System.out.println(); // Переход на новую строку
 
-        // Задача 7: Последовательность чисел 1, 2, 4, 8, 16, ..., 512
-        System.out.println("\nЗадача 7:");
-        for (int i = 1; i <= 512; i *= 2) {
-            System.out.print(i + " ");
+        // Цикл for
+        for (int j = 10; j >= 1; j--) {
+            System.out.print(j + " ");
         }
-        System.out.println(); // Для перевода строки после вывода
+        System.out.println();
+        System.out.println(); // Переход на новую строку
 
-        // Задача 8: Сумма накоплений без процентов (29 000 рублей каждый месяц)
-        System.out.println("\nЗадача 8:");
-        int monthlySavings = 29000;
-        for (int month = 1; month <= 12; month++) {
-            int totalSavings = monthlySavings * month;
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + totalSavings + " рублей");
+        // Задача 3: Рассчитать численность населения через 10 лет
+        System.out.println("Задача 3:");
+        int population = 12000000;  // Начальное население
+        double birthRate = 17.0 / 1000;  // Рождаемость
+        double deathRate = 8.0 / 1000;  // Смертность
+
+        for (int year = 1; year <= 10; year++) {
+            population += population * (birthRate - deathRate);
+            System.out.println("Год " + year + ", численность населения составляет " + (int) population);
         }
+        System.out.println(); // Переход на новую строку
 
-        // Задача 9: Сумма накоплений с 12% годовых
-        System.out.println("\nЗадача 9:");
-        double totalSavingsWithInterest = 0;
-        double interestRate = 0.12;
-        for (int month = 1; month <= 12; month++) {
-            totalSavingsWithInterest += monthlySavings;
-            totalSavingsWithInterest += totalSavingsWithInterest * (interestRate / 12);  // Ежемесячный процент
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + totalSavingsWithInterest + " рублей");
+        // Задача 4: Рассчитать, сколько месяцев Василию нужно копить, чтобы собрать 12 миллионов
+        System.out.println("Задача 4:");
+        double initialAmount = 15000;  // Начальная сумма
+        double targetAmount = 12000000;  // Цель накоплений
+        double currentAmount = initialAmount;  // Текущая сумма
+        double interestRate = 0.07;  // Процент на месяц
+
+        months = 0;
+
+        while (currentAmount < targetAmount) {
+            currentAmount += currentAmount * interestRate;
+            months++;
+            System.out.println("Месяц " + months + ", сумма накоплений равна " + currentAmount + " рублей");
         }
+        System.out.println("Для достижения цели потребуется " + months + " месяцев.");
+        System.out.println(); // Переход на новую строку
 
-        // Задача 10: Таблица умножения на 2
-        System.out.println("\nЗадача 10:");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("2 * " + i + " = " + (2 * i));
+        // Задача 5: Выводить накопления каждый шестой месяц
+        System.out.println("Задача 5:");
+        currentAmount = initialAmount;
+        months = 0;
+
+        while (currentAmount < targetAmount) {
+            currentAmount += currentAmount * interestRate;
+            months++;
+            if (months % 6 == 0) {
+                System.out.println("Месяц " + months + ", сумма накоплений равна " + currentAmount + " рублей");
+            }
+        }
+        System.out.println(); // Переход на новую строку
+
+        // Задача 6: Выводить сумму накоплений каждые полгода на протяжении 9 лет
+        System.out.println("Задача 6:");
+        currentAmount = initialAmount;
+        months = 0;
+
+        for (int year = 1; year <= 9; year++) {
+            for (int month = 1; month <= 6; month++) {
+                currentAmount += currentAmount * interestRate;
+                months++;
+            }
+            System.out.println("Полгода, год " + year + ", сумма накоплений равна " + currentAmount + " рублей");
+        }
+        System.out.println(); // Переход на новую строку
+
+        // Задача 7: Написать программу для напоминания о пятницах
+        System.out.println("Задача 7:");
+        int firstFriday = 3; // Допустим, первая пятница месяца - это 3-й день
+
+        for (int day = firstFriday; day <= 31; day += 7) {
+            System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет.");
+        }
+        System.out.println(); // Переход на новую строку
+
+        // Задача 8: Программирование вычисления лет, когда пролетает комета
+        System.out.println("Задача 8:");
+        int startYear = 1825; // 200 лет назад
+        int endYear = 2125; // 100 лет вперед
+
+        for (int year = startYear; year <= endYear; year++) {
+            if (year % 79 == 0) {
+                System.out.println(year);
+            }
         }
     }
 }
